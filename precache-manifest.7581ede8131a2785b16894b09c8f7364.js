28a9acff464451d79314715cfbb99341aac78493
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-06-16 10:18:27 +0200

    Updates

---
+++ precache-manifest.7581ede8131a2785b16894b09c8f7364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7baa22f5166cc951c3cc008fbfb38b1",
+    "url": "/LabReactSwapi/index.html"
+  },
+  {
+    "revision": "41c475c832155dfd3ab8",
+    "url": "/LabReactSwapi/static/css/main.41d81db5.chunk.css"
+  },
+  {
+    "revision": "b01c5e6ef7fc062cd0de",
+    "url": "/LabReactSwapi/static/js/2.d7e50a0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LabReactSwapi/static/js/2.d7e50a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c475c832155dfd3ab8",
+    "url": "/LabReactSwapi/static/js/main.1e42cc68.chunk.js"
+  },
+  {
+    "revision": "1ca425fb2986703d7a4e",
+    "url": "/LabReactSwapi/static/js/runtime-main.97bd2a1b.js"
+  },
+  {
+    "revision": "36486e5dc999e9f82154a01eb91fc058",
+    "url": "/LabReactSwapi/static/media/yodacat.36486e5d.png"
+  }
+]);
